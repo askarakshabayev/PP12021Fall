ab9d8f4d9fb7397e32e4cf74acd8be9c9c20799e
Askar Akshabayev <askarakshabayev@MacBook-Pro-Askar.local>
2021-09-04 13:54:35 +0600

    week1

---
+++ week1/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // cout << "hello world" << " abc";
+    // cout << "test";
+    // cout << "5 + 6 " << 5 + 6;
+    cout << "hello \nworld" << endl; 
+    cout << "abc\n";
+    cout << 5 + 6;
+    return 0;
+}
+
